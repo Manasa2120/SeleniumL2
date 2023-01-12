f756702888ff89d0f9886fa2c737a04762025ad9
KaranGowda21 <121274810+KaranGowda21@users.noreply.github.com>
2023-01-12 14:27:39 +0530

    Driver files added

--- WiproBasic/src/test/java/Wipro/WiproBasic/BasicSelenium.java
+++ WiproBasic/src/test/java/Wipro/WiproBasic/BasicSelenium.java
@@ -1,10 +1,17 @@
 package Wipro.WiproBasic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicSelenium {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\git\\SeleniumL2\\WiproBasic\\Driver\\chromedriver.exe" );
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
 	}
 
 }
